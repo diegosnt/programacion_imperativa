c216803b85c24f3b86ea1cac313d158843ebca42
Diego Santacruz <dsantacruz@gmail.com>
2022-05-27 13:51:27 -0300

    Primer commit

---
+++ 02.variables.js
@@ -0,0 +1,21 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+// VARIABLES
+let edad = 29;
+    edad = 30;
+    edad = 31;
+//CONSTANTE
+const nombre = "diego";
+//      nombre = "jose";
+
+//TIPOS DE DATOS
+// NUMBERS / STRING / BOOLEAN
+
+let estaCasado = !true; 
+
+// OPERADORES DE COMPARACION
+
+let numero1 = 12;
+let numero2 = "12";
+
+console.log(estaCasado);
